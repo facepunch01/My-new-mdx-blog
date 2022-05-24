7a3b16262db0838cbca06a0fc6fabfdcc4616355
thatboyjake <45983348+thatboyjake@users.noreply.github.com>
2022-05-24 18:51:52 -0500

    hero done

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import svelte from '@astrojs/svelte';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import lit from "@astrojs/lit";
-
 // https://astro.build/config
 export default defineConfig({
-  // Enable Svelte to support Svelte components.
   integrations: [svelte(), tailwind(), sitemap()]
 });
